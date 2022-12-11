a3a8f9dbc8ae394cfa33b4400d67728b42d1e51f
eellda <tree500253@gmail.com>
2022-12-11 21:03:07 +0900

    js input study

--- jam/jami.js
+++ jam/jami.js
@@ -9,9 +9,29 @@ document.getElementById("bt").onclick = function () {
     alert("버튼을 눌렀다고.");
 }
 
-// 인풋값 //
-let value = document.getElementById("ip").value;
-document.addEventListener('submit', function ipvalue() {
-    alert(ipvalue.value + '라고 적음');
+// 인풋 //
+// var ipprint = document.getElementById('ip').value;
+// function enterkey() { 
+// 	if (window.event.keyCode == 13) { 
+//         console.log(ipprint.value);
+//     } 
+// }
+
+//입력한 값을 div에 출력
+const ip = document.getElementById('ip');
+
+ip.addEventListener('keyup', function printName() {
+    const name = document.getElementById('ip').value;
+    document.getElementById('ip-result').innerText = name;
 })
+// function printName() {
+//     const name = document.getElementById('ip').value;
+//     document.getElementById('ip-result').innerText = name;
+// }
+// html - onkeyup="printName()"
+
+// document.getElementById('ip').focus();
+// let reset = document.getElementById('ip-reset');
+// reset.value = null;
+// reset.focus;
 
