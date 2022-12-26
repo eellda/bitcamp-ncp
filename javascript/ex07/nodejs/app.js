a71de047e918d97fc7cc435fc6e64dc84ab13493
eellda <tree500253@gmail.com>
2022-12-26 22:35:52 +0900

    key

--- javascript/ex07/nodejs/app.js
+++ javascript/ex07/nodejs/app.js
@@ -103,6 +103,14 @@ app.get('/proxy', (req, res) => {
     //console.log('오호라!');
 });
 
+app.get('/proxy', (req, res) => {                
+    //console.log(req.query.url);   
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Content-Type', 'application/json; charset=UTF-8');
+    request.get({url: req.query.url}, (error, response, body) => res.send(body));
+    //console.log('오호라!');
+});
+
 
 
 app.get('/proxy2', (req, res) => {                
@@ -122,6 +130,44 @@ app.get('/proxy2', (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+app.get('/proxy3', (req, res) => {                
+
+    res.set('Access-Control-Allow-Origin', '*');
+    res.set('Content-Type', 'application/json; charset=UTF-8');
+
+    let openApiUrl = 'https://api.odcloud.kr/api/WeeklyAptTrendSvc/v1/getAptTradingMarketTrend?' +
+    'page=1' +
+    '&perPage=1000' +
+    '&cond%5BREGION_CD%3A%3AEQ%5D=11A01' + 
+    '&cond%5BRESEARCH_DATE%3A%3ALTE%5D=' + date +
+    '&cond%5BRESEARCH_DATE%3A%3AGTE%5D=20180101' +
+    '&serviceKey=sxu9KRkG12s8M1jzMQue37aHfnGiREb4AkZ3vd3k6AyedQ%2Bwzi69FPFxjjhCFBRG%2Bg2yWdwfHlsp9ICw5TfOZQ%3D%3D';
+
+    request.get({url: openApiUrl}, (error, response, body) => res.send(body));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
 app.post('/login', (req, res) => {                                   // post 요청이 들어왔을때 실행할 함수를 등록 하는것임                                                    
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Content-Type', 'text/plain; charset=UTF-8');
@@ -132,10 +178,9 @@ password: ${req.body.password}`);
 
 
 
-
 // 웹서버 실행하기
 app.listen(
-    4000,                                                                               // 포트 번호 지정
+    3000,                                                                               // 포트 번호 지정
     () => {                                                                  
         console.log(`${port}번 포트에서 서버 구동 시작`);                               // 서버가 시작 되었을때 호출될 함수 = 리스너 = 핸들러
     }                                                     
