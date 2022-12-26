5b70b5fd521728b5313900f62fe28dc5959a4333
eellda <tree500253@gmail.com>
2022-12-27 00:27:52 +0900

    회원가입 및 업로드 제한 추가

--- firebase/public/js/login.js
+++ firebase/public/js/login.js
@@ -1,11 +1,28 @@
+$('#login').click(function () {
+
+    var email = $('#email').val();
+    var password = $('#pw').val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password).then((result) => {
+        console.log(result.user);
+    })
+})
+
+$('#logout').click(function () {
+    firebase.auth().signOut();
+})
+
+
 $('#register').click(function () {
 
     var email = $('#email-new').val();
     var password = $('#pw-new').val();
+    var name = $('#name-new').val();
 
     firebase.auth().createUserWithEmailAndPassword(email, password).then((result) => {
         console.log(result);
         console.log(result.user);
+        result.user.updateProfile({displayName: name})
     })
 })
 
