ac4d46158ee638aa959fce2d8b2274325b4e74a5
eellda <tree500253@gmail.com>
2022-12-29 21:25:59 +0900

    1229 java arr 및 leteral 문법

--- eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0530.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex04/Exam0530.java
@@ -6,7 +6,7 @@ public class Exam0530 {
   public static void main(String[] args) {
 
     int[] arr1 = new int[5];
-    
+
     // 배열 선언문 분석:
     // arr1
     //   - 배열 메모리의 주소를 담는 변수이다.
@@ -20,8 +20,8 @@ public class Exam0530 {
     //
     // new 명령은 메모리를 확보하는 명령이다.
     // - 리턴 값은 확보된 메모리의 시작 주소이다.
-    
-    // 
+
+
     arr1 = new int[5];
 
     arr1[0] = 100; // arr1에 저장된 주소로 찾아가서 0번째 항목에 값을 넣어라!
