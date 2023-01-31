cad9635d79ab1db22ecdb2a895409cd60dcfa8d9
eellda <tree500253@gmail.com>
2023-02-01 00:33:48 +0900

    Refactor : app
    GRASP pattern 중 Information Expert 에 따라
    변수만 따로 모아둔 Member class 를 생성
    Prompt 는 사용자 입출력 기능만 하고
    MemberHandler 는 input 과 print 만 하도록 함
    그리고 new 명령어를 통해 instance 를 생성하여 입출력 하도록 수정

--- myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
+++ myapp/app/src/main/java/bitcamp/myapp/MemberHandler.java
@@ -7,36 +7,26 @@ public class MemberHandler {
 	static final int SIZE = 100;
 	static int count = 0;
 
-	static int[] no = new int[SIZE];
-	static String[] name = new String[SIZE];
-	static String[] tel = new String[SIZE];
-	static String[] postNo = new String[SIZE];
-	static String[] basicAddress = new String[SIZE];
-	static String[] detailAddress = new String[SIZE];
-	static boolean[] working = new boolean[SIZE];
-	static char[] gender = new char[SIZE];
-	static byte[] level = new byte[SIZE];
-	static String[] createdDate = new String[SIZE];
+	static Member[] members = new Member[SIZE];
 
 	static void inputMembers() {
 
 		for (int i = 0; i < SIZE; i++) {
-			no[i] = Prompt.inputInt("번호? ");
-			name[i] = Prompt.inputString("이름? ");
-			tel[i] = Prompt.inputString("전화번호? ");
-			postNo[i] = Prompt.inputString("우편번호? ");
-			basicAddress[i] = Prompt.inputString("기본주소? ");
-			detailAddress[i] = Prompt.inputString("상세주소? ");
-			String title = "0. 백수\n1. 재직\n직업? ";
-			working[i] = Prompt.inputInt(title) == 1;
-			title = "0. 남자\n1. 여자\n성별? ";
-			gender[i] =  Prompt.inputInt(title) == 0 ? 'M' :  'W';
-			title = "0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ";
-			level[i] = (byte) Prompt.inputInt(title);
+			Member m = new Member();
+			m.no = Prompt.inputInt("번호? ");
+			m.name = Prompt.inputString("이름? ");
+			m.tel = Prompt.inputString("전화번호? ");
+			m.postNo = Prompt.inputString("우편번호? ");
+			m.basicAddress = Prompt.inputString("기본주소? ");
+			m.detailAddress = Prompt.inputString("상세주소? ");
+			m.working = Prompt.inputInt("0. 백수\n1. 재직\n직업? ") == 1;
+			m.gender =  Prompt.inputInt("0. 남자\n1. 여자\n성별? ") == 0 ? 'M' :  'W';
+			m.level = (byte) Prompt.inputInt("0. 비전공자\n1. 준전공자\n2. 전공자\n전공? ");
 
 			Date today = new Date(System.currentTimeMillis());
-			createdDate[i] = today.toString();
+			m.createdDate = today.toString();
 
+			members[i] = m;
 			count++;
 
 			String str = Prompt.inputString("계속 입력할거임? (Y/n)");
@@ -50,17 +40,18 @@ public class MemberHandler {
 
 	static void printMembers() {
 		for (int i = 0; i < count; i++) {
-			System.out.printf("번호: %d\n", no[i]);
-			System.out.printf("이름: %s\n", name[i]);
-			System.out.printf("전화: %s\n", tel[i]);
-			System.out.printf("우편번호: %s\n", postNo[i]);
-			System.out.printf("주소1: %s\n", basicAddress[i]);
-			System.out.printf("주소2: %s\n", detailAddress[i]);
-			System.out.printf("재직자: %s\n", working[i] ? "예" : "아니오");
-			System.out.printf("성별: %s\n", gender[i] == 'M' ? "남자" : "여자");
+			Member m = members[i];
+			System.out.printf("번호: %d\n", m.no);
+			System.out.printf("이름: %s\n", m.name);
+			System.out.printf("전화: %s\n", m.tel);
+			System.out.printf("우편번호: %s\n", m.postNo);
+			System.out.printf("주소1: %s\n", m.basicAddress);
+			System.out.printf("주소2: %s\n", m.detailAddress);
+			System.out.printf("재직자: %s\n", m.working ? "예" : "아니오");
+			System.out.printf("성별: %s\n", m.gender == 'M' ? "남자" : "여자");
 
 			String levelTitle;
-			switch (level[i]) {
+			switch (m.level) {
 			case 0: levelTitle = "비전공"; break;
 			case 1: levelTitle = "준전공"; break;
 			default:
@@ -68,7 +59,7 @@ public class MemberHandler {
 			}
 
 			System.out.printf("전공: %s\n", levelTitle);
-			System.out.printf("생성일: %s\n", createdDate[i]);
+			System.out.printf("생성일: %s\n", m.createdDate);
 			System.out.println("--------------------------------------");
 		}
 	}
