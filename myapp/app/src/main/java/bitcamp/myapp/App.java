fdd542b476aeb76d299f907832e4cd6d092e16c8
eellda <tree500253@gmail.com>
2023-01-31 23:33:15 +0900

    feat : app
    Myapp practice 에 배열 추가
    배열 크기는 final int SIZE = 100; 으로 선언
    배열 생성으로 입력 출력 for 문으로 변경
    다만 100번 다 입력하는건 아니기에 중간에 계속 입력 할건지 물어보도록 기능 추가

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -11,53 +11,83 @@ public class App {
 
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("번호? ");
-		int no = Integer.parseInt(scanner.nextLine());
-		System.out.print("이름? ");
-		String name = scanner.nextLine();
-		System.out.print("전화번호? ");
-		String tel = scanner.nextLine();
-		System.out.print("우편번호? ");
-		String postNo = scanner.nextLine();
-		System.out.print("기본주소? ");
-		String basicAddress = scanner.nextLine();
-		System.out.print("상세주소? ");
-		String detailAddress = scanner.nextLine();
-		System.out.println("0. 백수");
-		System.out.println("1. 재직");
-		System.out.print("재직여부? ");
-		boolean working = Integer.parseInt(scanner.nextLine()) == 1;
-		System.out.println("0. 남자");
-		System.out.println("1. 여자");
-		System.out.printf("성별? ");
-		char gender =  Integer.parseInt(scanner.nextLine()) == 0 ? 'M' :  'W';
-		System.out.println("0. 비전공");
-		System.out.println("1. 준전공");
-		System.out.println("2. 전공");
-		System.out.printf("전공? ");
-		byte level = Byte.parseByte(scanner.nextLine());
+		final int SIZE = 100;
+		int count = 0;
 
-		Date today = new Date(System.currentTimeMillis());
-		String createdDate = today.toString();
+		int[] no = new int[SIZE];
+		String[] name = new String[SIZE];
+		String[] tel = new String[SIZE];
+		String[] postNo = new String[SIZE];
+		String[] basicAddress = new String[SIZE];
+		String[] detailAddress = new String[SIZE];
+		boolean[] working = new boolean[SIZE];
+		char[] gender = new char[SIZE];
+		byte[] level = new byte[SIZE];
+		String[] createdDate = new String[SIZE];
 
-		System.out.printf("번호: %d\n", no);
-		System.out.printf("이름: %s\n", name);
-		System.out.printf("전화: %s\n", tel);
-		System.out.printf("우편번호: %s\n", postNo);
-		System.out.printf("주소1: %s\n", basicAddress);
-		System.out.printf("주소2: %s\n", detailAddress);
-		System.out.printf("재직자: %s\n", working ? "예" : "아니오");
-		System.out.printf("성별: %s\n", gender == 'M' ? "남자" : "여자");
+		for (int i = 0; i < SIZE; i++) {
+			System.out.print("번호? ");
+			no[i] = Integer.parseInt(scanner.nextLine());
+			System.out.print("이름? ");
+			name[i] = scanner.nextLine();
+			System.out.print("전화번호? ");
+			tel[i] = scanner.nextLine();
+			System.out.print("우편번호? ");
+			postNo[i] = scanner.nextLine();
+			System.out.print("기본주소? ");
+			basicAddress[i] = scanner.nextLine();
+			System.out.print("상세주소? ");
+			detailAddress[i] = scanner.nextLine();
+			System.out.println("0. 백수");
+			System.out.println("1. 재직");
+			System.out.print("재직여부? ");
+			working[i] = Integer.parseInt(scanner.nextLine()) == 1;
+			System.out.println("0. 남자");
+			System.out.println("1. 여자");
+			System.out.printf("성별? ");
+			gender[i] =  Integer.parseInt(scanner.nextLine()) == 0 ? 'M' :  'W';
+			System.out.println("0. 비전공");
+			System.out.println("1. 준전공");
+			System.out.println("2. 전공");
+			System.out.printf("전공? ");
+			level[i] = Byte.parseByte(scanner.nextLine());
 
-		String levelTitle;
-		switch (level) {
-		case 0: levelTitle = "비전공"; break;
-		case 1: levelTitle = "준전공"; break;
-		default:
-			levelTitle = "전공자";
+			Date today = new Date(System.currentTimeMillis());
+			createdDate[i] = today.toString();
+
+			count++;
+
+			System.out.println("계속 입력할거임? (Y/n)");
+			String str = scanner.nextLine();
+			if (!str.equalsIgnoreCase("Y") && str.length() != 0) {
+				break;
+			}
 		}
 
-		System.out.printf("전공: %s\n", levelTitle);
-		System.out.printf("생성일: %s\n", createdDate);
+		scanner.close();
+		System.out.println();
+
+		for (int i = 0; i < count; i++) {
+			System.out.printf("번호: %d\n", no[i]);
+			System.out.printf("이름: %s\n", name[i]);
+			System.out.printf("전화: %s\n", tel[i]);
+			System.out.printf("우편번호: %s\n", postNo[i]);
+			System.out.printf("주소1: %s\n", basicAddress[i]);
+			System.out.printf("주소2: %s\n", detailAddress[i]);
+			System.out.printf("재직자: %s\n", working[i] ? "예" : "아니오");
+			System.out.printf("성별: %s\n", gender[i] == 'M' ? "남자" : "여자");
+
+			String levelTitle;
+			switch (level[i]) {
+			case 0: levelTitle = "비전공"; break;
+			case 1: levelTitle = "준전공"; break;
+			default:
+				levelTitle = "전공자";
+			}
+
+			System.out.printf("전공: %s\n", levelTitle);
+			System.out.printf("생성일: %s\n", createdDate[i]);
+			System.out.println("--------------------------------------");
+		}
 	}
 }
