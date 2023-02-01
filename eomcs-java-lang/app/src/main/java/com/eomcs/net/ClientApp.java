2b06bda906dc9becb9836004b7ab8486fae9789e
eellda <tree500253@gmail.com>
2023-02-02 00:20:21 +0900

    Refactor : CalcClient, CaclServer
    ServerApp3, ClientApp3 과제 제출 조건으로 rename
    변수 명은 줄일 수 있는건 줄이고 좀 더 직관성 있게 변경함.
    나머지 refactoring 가능한 건 진행

--- eomcs-java-lang/app/src/main/java/com/eomcs/net/ClientApp.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/net/ClientApp.java
@@ -5,39 +5,39 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class ClientApp {
-  public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("클라 실행중 ~");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("클라 실행중 ~");
 
-    Socket socket = new Socket("192.168.0.5", 8888);
-    System.out.println("server 연결중");
+		Socket socket = new Socket("192.168.0.5", 8888);
+		System.out.println("server 연결중");
 
-    PrintStream out = new PrintStream(socket.getOutputStream());
-    Scanner in = new Scanner(socket.getInputStream());
+		PrintStream out = new PrintStream(socket.getOutputStream());
+		Scanner in = new Scanner(socket.getInputStream());
 
-    while (true) {
-      System.out.println("입력 >> ");
-      String message = scanner.nextLine();
-      out.println(message);
+		while (true) {
+			System.out.println("입력 >> ");
+			String message = scanner.nextLine();
+			out.println(message);
 
-      if (message.equals("quit")) {
-        break;
-      }
+			if (message.equals("quit")) {
+				break;
+			}
 
-      //      System.out.println("서버에 메세지 보냄");
+			//      System.out.println("서버에 메세지 보냄");
 
-      // 서버에서 응답이 올 때까지 리턴하지 않음.
-      String responese = in.nextLine();
-      System.out.printf("응답 : %s\n", responese);
-    }
+			// 서버에서 응답이 올 때까지 리턴하지 않음.
+			String responese = in.nextLine();
+			System.out.printf("응답 : %s\n", responese);
+		}
 
-    out.close();
-    in.close();
-    socket.close();
+		out.close();
+		in.close();
+		socket.close();
 
-    System.out.println("클라 종료");
-    scanner.close();
+		System.out.println("클라 종료");
+		scanner.close();
 
-  }
+	}
 }
